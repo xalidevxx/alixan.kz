5605848dee006f301f08a7483f7fa97aa551b43f
xalidevxx <xalidevxx@gmail.com>
2025-09-04 22:32:23 +0500

    fix: remove me img

--- components/widgets/Bio.vue
+++ components/widgets/Bio.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="flex items-center justify-between max-md:flex-col-reverse md:gap-10 max-md:gap-6">
-    <div class="max-w-[700px]">
-      <p class="page-title">{{ $t('bio.title') }}</p>
-      <p class="page-text">{{ $t('bio.text') }}</p>
-    </div>
-    <img src="/img/me.jpg" alt="Alikhan" loading="lazy" class="object-cover" />
+  <section>
+    <p class="page-title">{{ $t('bio.title') }}</p>
+    <p class="page-text">{{ $t('bio.text') }}</p>
   </section>
 </template>
 
